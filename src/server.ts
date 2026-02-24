ad0648f81519f51877769314a4d3d1e337cf9f2f
Ashish Kumar <ashishkr45943@gmail.com>
2026-02-24 23:55:09 +0530

    added db connection

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { env } from "./config.js";
 import app from "./app.js";
+import  "./database/index.js"
 
 app
 	.listen(env.port, () => {
 		console.log(`Server is running on port: ${env.port}`)
 	})
-	.on('error', (err) => console.log(`Error while starting the Server`));
+	.on('error', (err) => console.log(`Error while starting the Server ${err}`));
