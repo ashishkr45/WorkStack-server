15a0d3655609c51723b4457f18612e0ff2a15350
Ashish Kumar <ashishkr45943@gmail.com>
2026-02-24 23:58:28 +0530

    added cookie parser

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,31 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 
 import { env } from './config.js';
 import { ApiError, ErrorType } from './core/ApiError.js';
-import errorHandler from './middlewares/globalErrorHandler.js'; 
+import errorHandler from './middlewares/errorHandler.middeware.js'; 
+import routes from './routes/index.js';
 
 const app = express();
 
 app.use(helmet());
 app.use(
 	cors({
-		origin: env.coreUrl,
+		origin: env.corsUrl,
 		credentials: true,
 	}),
 );
 
-app.use(express.json({ limit: '10mb' }))
+app.use(express.json({ limit: '10mb' }));
+app.use(cookieParser())
 app.use('/api', routes);
+
+app.use(errorHandler);
 
 app.use((req, res, next) => {
 	next(new ApiError(ErrorType.NOT_FOUND, 'Route not found'));
 });
-
-app.use(errorHandler);
 
 export default app;
